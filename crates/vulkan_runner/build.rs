d9856257dc99ec2c8c73e59e6c0021721203029c
Tendsin Mende <tendsin@protonmail.com>
2022-01-29 14:54:10 +0100

    fix build script to create resources directory if not there

--- crates/vulkan_runner/build.rs
+++ crates/vulkan_runner/build.rs
@@ -1,11 +1,16 @@
 use spirv_builder::{MetadataPrintout, SpirvBuilder};
 
+
+const SHADER_DIR: &'static str = "../../resources/";
 fn main() {
     SpirvBuilder::new("../test_shader", "spirv-unknown-vulkan1.1")
         .print_metadata(MetadataPrintout::Full)
         .build()
         .expect("Failed to build test shader");
 
+    //If not there, create the resources folder
+    std::fs::create_dir_all(SHADER_DIR).expect("Failed to create resources dir");
+    
     //copy spirv file into resource folder
     std::fs::copy(
         "../../target/spirv-builder/spirv-unknown-vulkan1.1/release/deps/test_shader.spv.dir/module",
